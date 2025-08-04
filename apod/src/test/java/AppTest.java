b0c51d6200c05bd4cd04e1b705694fbb61e7c1a8
D.Y.Mazurin <d.y.mazurin@gmail.com>
2025-08-04 19:29:02 +0000

    ++++

---
+++ apod/src/test/java/AppTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous test :-)
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
